c736528242b2b5ae2dff6f3b50860c8724ffe466
Gary Gitton <gary.gitton@gmail.com>
2016-11-16 20:43:57 +0100

    create Attribute entity

---
+++ src/Entity/Attribute.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Hiboutik\Api\Client\Entity;
+
+/**
+ * Class Attribute
+ * @package Hiboutik\Api\Client\Entity
+ */
+class Attribute
+{
+    /**
+     * @var string
+     */
+    protected $attribute;
+
+    /**
+     * @var string
+     */
+    protected $newValue;
+
+    /**
+     * @return string
+     */
+    public function getAttribute()
+    {
+        return $this->attribute;
+    }
+
+    /**
+     * @param string $attribute
+     */
+    public function setAttribute($attribute)
+    {
+        $this->attribute = $attribute;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewValue()
+    {
+        return $this->newValue;
+    }
+
+    /**
+     * @param string $newValue
+     */
+    public function setNewValue($newValue)
+    {
+        $this->newValue = $newValue;
+    }
+}
