a21aae3efebe1539c43e50493547a0dd5108343c
Gary Gitton <gary.gitton@gmail.com>
2016-11-17 21:24:03 +0100

    Create Brand entity

---
+++ src/Entity/Brand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Hiboutik\Api\Client\Entity;
+
+/**
+ * Class Brand
+ * @package Hiboutik\Api\Client\Entity
+ */
+class Brand
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var int
+     */
+    protected $enabled;
+
+    /**
+     * @var int
+     */
+    protected $position;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
