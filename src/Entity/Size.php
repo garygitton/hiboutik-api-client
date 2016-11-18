368b93459662a818cfdc3291eb66483c9fa109d5
Gary Gitton <gary.gitton@gmail.com>
2016-11-18 23:28:58 +0100

    create size entity

---
+++ src/Entity/Size.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Hiboutik\Api\Client\Entity;
+
+/**
+ * Class Size
+ * @package Hiboutik\Api\Client\Entity
+ */
+class Size
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $barcode;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBarcode()
+    {
+        return $this->barcode;
+    }
+
+    /**
+     * @param string $barcode
+     */
+    public function setBarcode($barcode)
+    {
+        $this->barcode = $barcode;
+    }
+}
