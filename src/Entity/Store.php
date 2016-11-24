01d60048c2d7334401c271265803cb9379973dbd
Gary Gitton <gary.gitton@gmail.com>
2016-11-24 13:30:39 +0100

    create Store Entity

---
+++ src/Entity/Store.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Hiboutik\Api\Client\Entity;
+
+class Store
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var int
+     */
+    public $warehouseId;
+
+    /**
+     * @var string
+     */
+    public $defaultCurrency;
+
+    /**
+     * @var string
+     */
+    public $defaultPaymentMethod;
+
+    /**
+     * @var string
+     */
+    public $timeZone;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWarehouseId()
+    {
+        return $this->warehouseId;
+    }
+
+    /**
+     * @param int $warehouseId
+     */
+    public function setWarehouseId($warehouseId)
+    {
+        $this->warehouseId = $warehouseId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefaultCurrency()
+    {
+        return $this->defaultCurrency;
+    }
+
+    /**
+     * @param string $defaultCurrency
+     */
+    public function setDefaultCurrency($defaultCurrency)
+    {
+        $this->defaultCurrency = $defaultCurrency;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefaultPaymentMethod()
+    {
+        return $this->defaultPaymentMethod;
+    }
+
+    /**
+     * @param string $defaultPaymentMethod
+     */
+    public function setDefaultPaymentMethod($defaultPaymentMethod)
+    {
+        $this->defaultPaymentMethod = $defaultPaymentMethod;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTimeZone()
+    {
+        return $this->timeZone;
+    }
+
+    /**
+     * @param string $timeZone
+     */
+    public function setTimeZone($timeZone)
+    {
+        $this->timeZone = $timeZone;
+    }
+}
