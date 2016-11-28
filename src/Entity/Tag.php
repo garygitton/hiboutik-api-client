3c6047760bb08fc811db85c9bcbbd89b926986e1
Gary Gitton <gary.gitton@gmail.com>
2016-11-28 16:06:30 +0100

    create TAg Entity

---
+++ src/Entity/Tag.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Hiboutik\Api\Client\Entity;
+
+/**
+ * Class Tag
+ * @package Hiboutik\Api\Client\Entity
+ */
+class Tag
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
